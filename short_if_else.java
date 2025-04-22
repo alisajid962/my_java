ce8d4fd8f7855eab9eab3d21972d740451783edf
ALI Sajid <antihacker342@gmail.com>
2025-04-22 23:18:49 +0500

    Add files via upload
    Short-hand if else

---
+++ short_if_else.java
@@ -0,0 +1,9 @@
+public class short_if_else {
+    public static void main(String[] args) {
+        int a =12;
+        int b = 23;
+        String result = (a>b) ?  "a is grater than b" : "b is greater than a";
+        System.out.println(result);
+
+    }
+}
