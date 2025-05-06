6457951149dbad7e244d89de16ae03b80af2e24f
ALI Sajid <antihacker342@gmail.com>
2025-05-06 23:21:30 +0500

    Add files via upload

---
+++ Array_list.java
@@ -0,0 +1,39 @@
+
+import java.util.ArrayList;
+
+public class Array_list {
+
+    public static void main(String[] args) {
+        ArrayList<String> days = new ArrayList<>();
+        days.add("Monday");
+        days.add("tueday");
+        days.add("Wednesday");
+        days.add("Thursday");
+        days.add("Friday");
+        days.add("Saturday");
+        days.add("Sunday");
+        ArrayList<Integer> expenses = new ArrayList<>();
+        expenses.add(1200);
+        expenses.add(800);
+        expenses.add(950);
+        expenses.add(1300);
+        expenses.add(760);
+        expenses.add(1100);
+        expenses.add(980);  // Example data
+        int total = 0;
+        int max = expenses.get(0);
+        int maxIndex = 0;
+
+        for (int i = 0; i < expenses.size(); i++) {
+            total += expenses.get(i);
+            if (expenses.get(i) > max) {
+                max = expenses.get(i);
+                maxIndex = i;
+
+            }
+        }
+        System.out.println("Total weekly expense: " + total);
+        System.out.println("Highest expense on: " + days.get(maxIndex) + " (" + max + ")");
+
+    }
+}
