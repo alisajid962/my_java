2aae8f7fe223bf860ff8f223bb03f6977dd8f2b8
ALI Sajid <antihacker342@gmail.com>
2025-05-16 23:25:16 +0500

    Update University_Management_System.java

--- UMS/University_Management_System.java
+++ UMS/University_Management_System.java
@@ -4,7 +4,8 @@ public class University_Management_System{
     Department department1 = new Department("Computer Science", 101);
     Department department2 = new Department("Mathematics", 102);
     Department department3 = new Department("Physics", 103);
-    Department[] departments = {department1, department2, department3};
+        Department cs = new Department("java",204)
+    Department[] departments = {department1, department2, department3,cs};
     University university = new University("ABC University", departments);
     university.display();
 
