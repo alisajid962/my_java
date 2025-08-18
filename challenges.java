98748572400b7481fcbd18585fce535ddc933b41
ALI Sajid <antihacker342@gmail.com>
2025-08-18 23:59:26 +0500

    Add files via upload

---
+++ challenges.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class challenges {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter the number: " );
+//        int n = sc.nextInt();
+//       int  bitmask = 1;
+//        if((n & 1)==0){
+//            System.out.println("the number is even");
+//        }
+//        else{
+//            System.out.println("The number n is oddd");
+//        }
+
+        //get
+//        int bitmask = 1<<2;
+//        if( (n & bitmask) == 0){
+//            System.out.println("0");
+//        }
+//        else{
+//            System.out.println(1);
+//        }
+
+        // set
+//        int m = 10;//1010
+//        int bitmask = 1;
+//        System.out.println(m|bitmask);
+
+
+//        int m = 14;
+//        int bitmask = 1<<1;
+//        int notbitmask = ~(bitmask);
+//        System.out.println(m&notbitmask);
+
+
+
+
+    }
+}
